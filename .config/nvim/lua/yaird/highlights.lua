89085d1c7f43b3537b598ccb767aaa6dd4423c3d
yairdyule <jaje9434@colorado.edu>
2022-12-11 11:47:44 -0700

    fix(nvim): `Folded` hl group

--- .config/nvim/lua/yaird/highlights.lua
+++ .config/nvim/lua/yaird/highlights.lua
@@ -2,6 +2,6 @@ vim.api.nvim_create_autocmd({ "ColorScheme" }, {
 	pattern = "*",
 	callback = function()
 		vim.api.nvim_set_hl(0, "CursorLine", {})
-		vim.api.nvim_set_hl(0, "Folded", {})
+		vim.api.nvim_set_hl(0, "Folded", {fg="#626880"})
 	end,
 })
